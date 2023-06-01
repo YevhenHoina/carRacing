11db8f372cbdc72bed281c422c088016cadfc3ab
YevhenHoina <60106209+YevhenHoina@users.noreply.github.com>
2023-06-01 21:15:03 +0300

    open/close gamemenu in game

--- main.py
+++ main.py
@@ -13,24 +13,36 @@ def set_difficulty(value, difficulty):
     pass
 
 def start_the_game():
+    run=True
     while run:
         GAME = pygame.display.set_mode((1280, 720))
         print("game is running!")
+        for event in pygame.event.get():
+            if(event.type == pygame.KEYDOWN):
+                if(event.key == pygame.K_SPACE):
+                    menu=pygame_menu.Menu('Pause', 690, 360,
+                    theme=pygame_menu.themes.THEME_SOLARIZED)
+                    menu.add.button('Back', open_menu,
+                                    align=pygame_menu.locals.ALIGN_CENTER,
+                                    font_name = "arialblack")
+                    menu.mainloop(GAME)            
     pass
 
-
-
-menu = pygame_menu.Menu('Welcome', 1280, 720,
+def open_menu():
+    run = False
+    menu = pygame_menu.Menu('Welcome', 1280, 720,
                     theme=pygame_menu.themes.THEME_SOLARIZED)
 
-menu.add.text_input('Name :', default='KNU Forever!!!',
-                font_name = "arialblack")
-menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty, 
-                font_name = "arialblack")
-menu.add.button('Play', start_the_game,
-                font_name = "arialblack")
-menu.add.button('Quit', pygame_menu.events.EXIT,
-                font_name = "arialblack",
-                font_color="red")
+    menu.add.text_input('Name :', default='KNU Forever!!!',
+                    font_name = "arialblack")
+    menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty, 
+                    font_name = "arialblack")
+    menu.add.button('Play', start_the_game,
+                    font_name = "arialblack")
+    menu.add.button('Quit', pygame_menu.events.EXIT,
+                    font_name = "arialblack",
+                    font_color="red")
 
-menu.mainloop(MENU)
+    menu.mainloop(MENU)
+
+open_menu()
