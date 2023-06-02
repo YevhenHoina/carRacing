05fa2c8d2a7913292bf7102018dff768985d86c9
YevhenHoina <60106209+YevhenHoina@users.noreply.github.com>
2023-06-02 12:35:14 +0300

    create base level

--- main.py
+++ main.py
@@ -7,25 +7,50 @@ MENU = pygame.display.set_mode((1280, 720))
 
 pygame.display.set_caption("Racing game")
 run = True
+car = pygame.image.load("Assets\\car.png").convert()
+car = pygame.transform.scale(car, (64, 128))
+
+frameRate = pygame.time.Clock()
 
 def set_difficulty(value, difficulty):
     # Do the job here !
     pass
 
 def start_the_game():
+    X = 608
+    Y = 0
     run=True
+    GAME = pygame.display.set_mode((1280, 720))
+    pygame.draw.rect(GAME, (54, 54, 54), pygame.Rect(240, 0, 800, 720))
+    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(625, 0, 10, 720))
+    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(645, 0, 10, 720))
+    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, 0, 10, 64))
+    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, 0, 10, 64))
+    line1 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, 0, 10, 64))
+    line2 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, 0, 10, 64))
+
     while run:
-        GAME = pygame.display.set_mode((1280, 720))
         print("game is running!")
         for event in pygame.event.get():
             if(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_SPACE):
                     menu=pygame_menu.Menu('Pause', 690, 360,
                     theme=pygame_menu.themes.THEME_SOLARIZED)
+                    menu.add.button('Resume', start_the_game,
+                                    align=pygame_menu.locals.ALIGN_CENTER,
+                                    font_name = "arialblack")
                     menu.add.button('Back', open_menu,
                                     align=pygame_menu.locals.ALIGN_CENTER,
                                     font_name = "arialblack")
-                    menu.mainloop(GAME)            
+                    menu.mainloop(GAME)   
+
+            Y+=1
+            GAME.blit(car, (X, 570))
+            pygame.display.flip()
+            frameRate.tick(128)
+
+                             
+
     pass
 
 def open_menu():
