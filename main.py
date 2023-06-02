92b93303ee6a41af2c8997a0c6115e724f91901a
YevhenHoina <60106209+YevhenHoina@users.noreply.github.com>
2023-06-02 13:25:06 +0300

    inicialize movement

--- main.py
+++ main.py
@@ -15,23 +15,42 @@ frameRate = pygame.time.Clock()
 def set_difficulty(value, difficulty):
     # Do the job here !
     pass
-
-def start_the_game():
-    X = 608
-    Y = 0
-    run=True
-    GAME = pygame.display.set_mode((1280, 720))
+def genFrame(GAME):
     pygame.draw.rect(GAME, (54, 54, 54), pygame.Rect(240, 0, 800, 720))
     pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(625, 0, 10, 720))
     pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(645, 0, 10, 720))
-    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, 0, 10, 64))
-    pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, 0, 10, 64))
-    line1 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, 0, 10, 64))
-    line2 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, 0, 10, 64))
+    
+def start_the_game():
+    X = 608
+    Y = -100
 
+    movement = 0
+    run=True
+    
+    GAME = pygame.display.set_mode((1280, 720))
+    line1 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, -100, 10, 64))
+    line2 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, -100, 10, 64))
+    line3 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, -475, 10, 64))
+    line4 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, -475, 10, 64))
+    
     while run:
+        GAME.fill("Black")
         print("game is running!")
+        genFrame(GAME)
+        line1 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(430, Y, 10, 64))
+        line2 = pygame.draw.rect(GAME, (255, 255, 255), pygame.Rect(835, Y, 10, 64))
+        pygame.draw.rect(GAME, (255, 255, 255), line1)
+        pygame.draw.rect(GAME, (255, 255, 255), line2)
+        GAME.blit(car, (X, 570))
+        X+=1*movement
+        Y+=1
+        if Y == 720:
+            Y = -100
+        pygame.display.update()
+
         for event in pygame.event.get():
+            
+            
             if(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_SPACE):
                     menu=pygame_menu.Menu('Pause', 690, 360,
@@ -42,10 +61,18 @@ def start_the_game():
                     menu.add.button('Back', open_menu,
                                     align=pygame_menu.locals.ALIGN_CENTER,
                                     font_name = "arialblack")
-                    menu.mainloop(GAME)   
-
-            Y+=1
-            GAME.blit(car, (X, 570))
+                    menu.mainloop(GAME)
+                if (event.key == pygame.K_RIGHT):
+                    movement = 0.5
+                if (event.key == pygame.K_LEFT):
+                    movement = -0.5
+            if(event.type == pygame.KEYUP):
+                if (event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT):
+                    movement = 0
+                    
+                    
+            
+            
             pygame.display.flip()
             frameRate.tick(128)
 
